39b15fdc701641e657f96a780edb7104963b7f47
ndotokubwa <163364083+ndotokubwa@users.noreply.github.com>
2024-10-12 19:07:32 +0300

    Add files via upload

---
+++ gggg/js/TOV39LKAIHs1.js
@@ -0,0 +1,31 @@
+const pwa_custom =`
+
+<div class="pl763847__wrap pl763847__bounceInUp ">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&amp;display=swap" rel="stylesheet">
+  <div class="pl763847__closelink">
+    <img src="https://loadingscripts.com/progress_p/pwa_links/adult/dating/msngr_1/2/close.png">
+  </div>
+  <div class="pl763847__content pl763847__btn">
+
+    <div class="pl763847__cont" >
+
+      <div class="pl763847__desc">
+        <div class="pl763847__pictire">
+          <img src="https://loadingscripts.com/progress_p/pwa_links/adult/dating/msngr_1/2/adobe_flash_player.png" alt="" class="pl763847__img">
+        </div>
+        <div class="pl763847__message">
+          <p class="pl763847__name__abonem">WhatsFuck</p>
+          <p class="pl763847__message__desc">Jenn</p>
+          <p class="pl763847__message__desc">Do you like my body? 😉</p>
+          <img src="https://loadingscripts.com/progress_p/pwa_links/adult/dating/msngr_1/2/icon.gif" class="pl763847__icon">
+          <p class="pl763847__message__desc-bottom">Notifications • via Web App</p>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+  `;
+
+    document.querySelector('body').insertAdjacentHTML('afterbegin', pwa_custom);
